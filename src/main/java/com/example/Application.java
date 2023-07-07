deaa5ec0c77ec4e8a7daef410bf69a1d86f63237
d-kuz <d_kuzyakova@mail.ru>
2023-07-07 16:30:33 +0300

    проект: вход, выход, частично корзина

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,11 @@
-package com.github.dkuz.mininotes;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan({"com.github.dkuz.mininotes.controller"})
+@ComponentScan("com.example.controller")
 public class Application {
     public static void main(String[] args) {
         System.setProperty("server.port", "8081");
--- src/main/java/com/github/dkuz/mininotes/repository/NoteRep.java
+++
@@ -1,9 +0,0 @@
-package com.github.dkuz.mininotes.repository;
-
-
-import com.github.dkuz.mininotes.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface NoteRep extends JpaRepository<Note, Long> {
-}
